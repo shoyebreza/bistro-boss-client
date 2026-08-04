73ce3809b5e88a73b696ff35b03b79d2ffb10448
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-08-04 22:26:41 +0600

    initial setup

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>&copy; 2023 My App. All rights reserved.</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Main from '../layout/Main.jsx'
+import Home from '../pages/Home/Home.jsx'
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    },
+]);
