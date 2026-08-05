906ba5a3e056f766037dd9146de3f53cb7d2bedd
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-08-05 07:51:47 +0600

    footer component

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router";
+import Footer from "../pages/Shared/Footer/Footer";
+
+
 const Main = () => {
     return ( 
         <div>
-            <h1>Main Layout</h1>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
      );
 }
