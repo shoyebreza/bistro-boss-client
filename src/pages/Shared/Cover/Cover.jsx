24b6919aa614634a417324840b6909eee5e6761c
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-08-07 23:21:00 +0600

    parallax

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,23 +1,28 @@
-const Cover = ({img, title}) => {
+import { Parallax } from "react-parallax";
+
+const Cover = ({ img, title }) => {
     return (
-        <div
-            className="hero min-h-screen h-[600px]"
-            style={{
-                backgroundImage: `url(${img})`,
-            }}
+        <Parallax
+            blur={{ min: -50, max: 50 }}
+            bgImage={img}
+            bgImageAlt="Cover Image"
+            strength={-200}
         >
-            <div className="hero-overlay"></div>
-            <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-                    <p className="mb-5">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    <button className="btn btn-primary">Get Started</button>
+            <div
+                className="hero min-h-screen h-[600px]">
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax >
     );
 }
 
