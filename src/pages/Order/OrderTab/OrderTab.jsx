da3082fce3d905c807f0fdd36e62bb3b6c1c651e
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-08-14 00:04:26 +0600

    pagination with swiper slide

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,11 +1,35 @@
 import FoodCard from "../../../components/FoodCard/FoodCard";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
-const OrderTab = ({items}) => {
+const OrderTab = ({ items }) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 my-10">
-            {
-                items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
-            }
+        <div>
+
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={24}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    768: {
+                        slidesPerView: 2,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                    },
+                }}
+                modules={[Pagination]}
+                className="mySwiper my-10"
+            >
+                {items.map(item => (
+                    <SwiperSlide key={item._id}>
+                        <FoodCard item={item} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
         </div>
     );
 }
