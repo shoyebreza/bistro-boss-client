1e887a54aa589c93fda70295be500fd9386be9ce
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-08-15 07:27:19 +0600

    added form captcha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useRef, useState } from "react";
 import { Helmet } from "react-helmet-async";
-
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from "react-simple-captcha";
 const Login = () => {
+
+    const captchaRef = useRef(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleValidateCaptcha = (e) => {
+        e.preventDefault();
+        const userCaptchaValue = captchaRef.current.value;
+        if (validateCaptcha(userCaptchaValue)) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+    }
+
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.target;
@@ -8,6 +24,10 @@ const Login = () => {
         const password = form.password.value;
         // Handle login logic here
     };
+
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, []);
     return (
         <div>
             <Helmet>
@@ -29,8 +49,12 @@ const Login = () => {
                                 <input type="email" name="email" className="input" placeholder="Email" />
                                 <label className="label">Password</label>
                                 <input type="password" name="password" className="input" placeholder="Password" />
+                                <label className="label"><LoadCanvasTemplate /></label>
+                                <input type="text" ref={captchaRef} name="captcha" className="input" placeholder="Enter Captcha" />
+                                <button type="button" className="btn btn-outline btn-xs mt-2" onClick={handleValidateCaptcha}>Validate</button>
+                                <div><a className="link link-hover">New user? Register here</a></div>
                                 <div><a className="link link-hover">Forgot password?</a></div>
-                                <button  type="submit" className="btn btn-neutral mt-4">Login</button>
+                                <button type="submit" disabled={disabled} className="btn btn-neutral mt-4">Login</button>
                             </fieldset>
                         </form>
                     </div>
