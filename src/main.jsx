e0a00b83a3200c9b34fef7f5e46a8553065e099e
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-05 22:12:35 +0600

    get cart data using tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes.jsx';
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from './Providers/AuthProvider.jsx';
-
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router}></RouterProvider>
-        </div>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router}></RouterProvider>
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
