62441f1ff82d34d0217b9862d7abff3320ea28aa
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-08 23:21:34 +0600

    looping item in cart

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -41,7 +41,7 @@ const Dashboard = () => {
                 </ul>
             </div>
             {/* dashboard content  */}
-            <div className="flex-1 p-4">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { FaTrash } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
 
 
@@ -5,10 +6,45 @@ const Cart = () => {
     const [cart] = useCart();
 
     return (
-        <div className="flex justify-center items-center flex-col gap-4">
-            <h2 className="text-4xl">Items : {cart.length}</h2>
-            <h2 className="text-4xl">Total Price : {cart.reduce((total, item) => total + (item.price * item.quantity), 0)}</h2>
-            <button className="btn btn-primary">Pay Now</button>
+        <div>
+            <div className="flex justify-evenly items-center gap-8">
+                <h2 className="text-4xl">Items : {cart.length}</h2>
+                <h2 className="text-4xl">Total Price : {cart.reduce((total, item) => total + (item.price * item.quantity), 0)}</h2>
+                <button className="btn btn-primary">Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => (
+                                <tr key={item._id}>
+                                    <td>
+                                        #{index + 1}
+                                    </td>
+                                    <td><img src={item.image} alt={item.name} className="w-16 h-16 object-cover" /></td>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}</td>
+                                    <th>
+                                        <button className="btn btn-warning btn-xs"><FaTrash /></button>
+                                    </th>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
