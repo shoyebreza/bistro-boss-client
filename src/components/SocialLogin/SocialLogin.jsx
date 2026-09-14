8dc34b2abc0c71c538bc5da52120968b468a2b1b
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-14 07:14:12 +0600

    catch user from social login

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,12 +1,28 @@
 import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router";
 
 const SocialLogin = () => {
     const { signInWithGoogle } = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
+
+
     const handleGoogleSignIn = () => {
         signInWithGoogle()
             .then(result => {
                 console.log(result);
+                const loggedUser = result.user;
+                const userInfo = { name: loggedUser.displayName, email: loggedUser.email };
+                axiosPublic.post('/users', userInfo)
+                    .then(res => {
+                        console.log(res);
+                        navigate('/');
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
             })
             .catch(error => {
                 console.error(error);
