7f07b0c2d515b59c5cc7981dffdef9076e07490e
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-18 20:41:03 +0600

    creating token in local storage

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
     updateProfile
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app)
@@ -19,6 +20,10 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+
+    const axiosPublic = useAxiosPublic();
+
+
     const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
@@ -51,9 +56,20 @@ const AuthProvider = ({ children }) => {
             setUser(currentUser);
             if(currentUser){
                 // get token and store in local storage
+                const userInfo = {
+                    email: currentUser.email
+                };
+                axiosPublic.post('/jwt', userInfo)
+                .then(res =>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
+                .catch(error => console.log(error))
 
             }else{
                 // TODO: remove token from local storage
+                localStorage.removeItem('access-token');
             }
             console.log('current user', currentUser);
             setLoading(false);
