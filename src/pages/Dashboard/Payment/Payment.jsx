57612404f00941c7d7579d62e7db143ce6b659f1
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-26 21:32:21 +0600

    stripe added

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -4,14 +4,14 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
 // TODO: add publishable key
-const stripePromise = [] /* loadStripe(import.meta.env.VITE_Payment_Gateway_PK); */
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
             <div>
                 <Elements stripe={stripePromise}>
-                    {/* <CheckoutForm></CheckoutForm> */}
+                    <CheckoutForm></CheckoutForm>
                 </Elements>
             </div>
         </div>
